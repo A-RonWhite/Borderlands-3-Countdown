7d8c7749484050fb3bf22d4f9aabf69ebe78e4d1
Aaron White <aaronwhite1234@hotmail.co.uk>
2019-05-22 14:42:09 +0700

    add audio and transitions

--- app.js
+++ app.js
@@ -17,14 +17,40 @@ const counter = setInterval(() => {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById(
-    'counter'
-  ).innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s `;
+  // Display the result
+  document.querySelector('.counter').innerHTML = `
+  <div class="col">
+          <p class="title">Days</p>
+          <p class="value">${days}</p>
+        </div>
+        <div class="col">
+          <p class="title">Hours</p>
+          <p class="value">${hours}</p>
+        </div>
+        <div class="col">
+          <p class="title">Minutes</p>
+          <p class="value">${minutes}</p>
+        </div>
+        <div class="col">
+          <p class="title">Seconds</p>
+          <p class="value">${seconds}</p>
+        </div>
+  `;
 
-  // If the count down is finished, write some text
+  // If the count down is finishes
   if (distance < 0) {
     clearInterval(counter);
-    document.getElementById('demo').innerHTML = 'EXPIRED';
+    document.querySelector('.counter').innerHTML =
+      '<h1>Stop checking the countdown and go play Borderlands</h1>';
   }
 }, 1000);
+
+const minionAudio = new Audio(
+  'audio/VO_Ep13_Pt2_02_echoX_Claptrap_1-minion.mp3'
+);
+
+setTimeout(() => {
+  document.querySelector('#claptrap').style.transform = 'translateY(20px)';
+}, 2500);
+
+setTimeout(() => minionAudio.play(), 3600);
